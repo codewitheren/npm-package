a3fd4d123430dd3543e2fe9320b269552e98fd8e
Mustafa Eren Erdoğan <mustafaerenerdogan@Mustafas-MacBook-Pro.local>
2024-02-27 17:35:36 +0300

    npm package

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# gmath
+basit maths library
+=======
+# Getir Math NPM Package
+>>>>>>> 661479f (npm package)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './func/isprime';
+export * from './func/iseven';
+export * from './func/isodd';
+export * from './func/fibonacci';
+export * from './func/factorial';
